faef4b2bcbde8145f6da4c855972c94adef95328
aa6488 <150643187+aa6488@users.noreply.github.com>
2024-01-05 19:08:14 +0900

    [level unrated] Title: k의 개수, Time: 48.26 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.java
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.java
@@ -1,10 +1,13 @@
 class Solution {
     public int solution(int i, int j, int k) {
+        // k 를 문자열로 변경
         String kk = k+"";
 		int answer = 0;
 		
 		for(int t = i; t<=j; t++) {
+            // 두자리 수 일수도 있기때문에 스트링으로 만든뒤 공백으로 split 한다.
 			String[] a = String.valueOf(t).split("");
+            // 배열로 자른 a 를  a의 길이만큼 반복해서 kk와 같을시 answer 를 +한다
 			for(int q = 0; q<a.length; q++) {
 				if(a[q].equals(kk)) {
 					answer++;
